73f003d3db25b12209aa55ffa9bd9a617bca0de0
YELANDAOKONG <yelandaokong@yldk.xyz>
2023-09-27 20:10:12 +0800

    init

--- recv.py
+++ recv.py
@@ -5,6 +5,7 @@ s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 while True:
     d, a = s.recvfrom(8192)
+    if a[1] != 65500 and a[1] != 65000: continue
     if d.decode("UTF-8") == "#FC":
         s.sendto(b"#RET", (a[0], 65000))
         continue
--- send.py
+++ send.py
@@ -1,5 +1,6 @@
 from socket import *
 s = socket(AF_INET, SOCK_DGRAM)
+s.bind(("0.0.0.0", 65500))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 n = input("Name=> ")
 bd = ("255.255.255.255", 65000)
@@ -8,6 +9,15 @@ while True:
     if not m: continue
     if m == "#FC":
         s.sendto(b"#FC",bd)
+        continue
+    if m.startswith("#P"):
+        ml = m.split(" ")
+        if len(ml) > 1:
+            bd = (ml[1], 65000)
+            print(bd)
+        elif len(ml) == 1:
+            bd = ("255.255.255.255", 65000)
+            print(bd)
         continue
     m = n + ": " + m
     s.sendto(m.encode("UTF-8"), bd)
